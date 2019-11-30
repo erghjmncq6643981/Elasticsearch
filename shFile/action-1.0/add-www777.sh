a24f3fbbed1a4cf8e0b366be00b7a10b25fad766
erghjmncq6643981 <erghjmncq6643981@163.com>
2019-11-30 15:42:55 +0800

    简单交互

---
+++ shFile/action-1.0/add-www777.sh
@@ -0,0 +1,10 @@
+ADDRESS=122.152.219.156:9200
+curl -s -H "Content-Type: application/json; charset=utf-8" -X PUT "$ADDRESS/commodity-info/_doc/www777?pretty" -d'{
+  "type_num": "碳酸饮料",
+  "name": "可口可乐",
+  "category_type_num": "饮料",
+  "storehouse_num": "1号仓库",
+  "brand": "可口可乐公司",
+  "manufacture_date": "2019-11-24",
+  "status": "销售"
+}'
