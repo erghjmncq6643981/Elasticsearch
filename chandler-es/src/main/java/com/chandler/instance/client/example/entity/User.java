48b1a21e5f57202a7f1d6523c8a29b19ccc29bdc
qiandingjun <erghjmncq6643981@163.com>
2020-01-12 15:35:14 +0800

    action 2.1

---
+++ chandler-es/src/main/java/com/chandler/instance/client/example/entity/User.java
@@ -0,0 +1,21 @@
+/*
+ * chandler-es
+ * 2020-01-12 14:50
+ *
+ * Please contact chandler
+ * if you need additional information or have any questions.
+ * Please contact chandler Corporation or visit:
+ * https://www.jianshu.com/u/117796446366
+ * @author 钱丁君-chandler
+ * @version 1.0
+ */
+package com.chandler.instance.client.example.entity;
+
+/**
+ * 类功能描述
+ *
+ * @author 钱丁君-chandler 2020-01-12 14:50
+ * @since 1.8
+ */
+public class User {
+}
